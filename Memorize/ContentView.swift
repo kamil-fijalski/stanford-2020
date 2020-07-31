1bb3484ffacd15622c14d6e0cb798e5172dd77e4
kamil-fijalski <kamil.fijalski@outlook.com>
2020-07-31 18:41:50 +0200

    lesson 1: initial layout

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,7 +10,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World x5!")
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+            .padding()
+            .font(Font.largeTitle)
+            .foregroundColor(Color.orange)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
     }
 }
 
