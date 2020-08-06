6c90fe259d4b523aa5e4f70b7bda17477614ad8d
kamil-fijalski <kamil.fijalski@outlook.com>
2020-08-06 18:22:23 +0200

    lesson 2: creating a MVVC prototype

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,32 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Kamil Fijalski on 06/08/2020.
+//  Copyright © 2020 FunctionReality. All rights reserved.
+//
+
+import Foundation
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["🏆","🍒","🌎"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+
+    //MARK: - Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    //MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
